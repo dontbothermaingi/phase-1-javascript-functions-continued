2430f7e78c843f45b537495347c4f068b7c4e650
+ <+>
2023-12-12 12:15:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+        return `This Saturday, I want to ${activity}!`
+
+}
+function mondayWork(plan = "go to the office") {
+    return `This Monday, I will ${plan}.`;
+}
+function wrapAdjective(flair = "*"){
+    return function (sed = "Special"){
+        return `You are ${flair}${sed}${flair}!`;
+    };
+}
